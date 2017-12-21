f27469636a84c988dfc065d64563784c9fe0ba49
Pavel Kirikov <lispad@gmail.com>
2017-12-21 13:26:26 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Time library
+
+Installation
+---
+
+Install the latest version with composer, you have to add a [private VCS repository](https://getcomposer.org/doc/05-repositories.md#using-private-repositories) as described below:
+
+```json
+{
+  "require": {
+    "clickadu/time": "*"
+  },
+  "repositories": [
+    {
+      "type": "vcs",
+      "url":  "git@github.com:WinnerSoftLab/time.git"
+    }
+  ]
+}
+```
---
+++ src/Time.php
@@ -0,0 +1,31 @@
+<?php
+namespace Clickadu\Time;
+
+/**
+ * Class Time
+ * @package Clickadu\Time
+ */
+final class Time
+{
+    const SECOND = 1;
+
+    const MINUTE = 60;
+
+    const HOUR = 3600;
+
+    const TWO_HOURS = 7200;
+
+    const THREE_HOURS = 10800;
+
+    const SIX_HOURS = 21600;
+
+    const DAY = 86400;
+
+    const TWO_DAYS = 172800;
+
+    const WEEK = 604800;
+
+    const MONTH = 2592000;
+
+    const YEAR = 31536000;
+}
